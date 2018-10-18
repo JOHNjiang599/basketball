6303ed0338ede7c8d7723486c776a61d9af39b44
weixingfu <wxffxw.664587@gmail.com>
2018-10-18 18:05:59 +0800

    update something

--- src/main/java/org/octopusbaby/basketball/dao/AdminDao.java
+++ src/main/java/org/octopusbaby/basketball/dao/AdminDao.java
@@ -3,6 +3,14 @@ package org.octopusbaby.basketball.dao;
 import org.apache.ibatis.annotations.Param;
 
 public interface AdminDao {
-
+    /**
+     * 添加管理员
+     *
+     * @param user
+     * @param password
+     * @return
+     */
     boolean addAdmin(@Param("user") String user, @Param("password") String password);
+
+
 }
--- src/main/java/org/octopusbaby/basketball/dao/TeamDao.java
+++ src/main/java/org/octopusbaby/basketball/dao/TeamDao.java
@@ -4,6 +4,8 @@ import org.octopusbaby.basketball.entity.Team;
 
 public interface TeamDao {
 
+    boolean addTeam();
+
     Team queryAllTeam();//查询所有球队信息
 
     Team findById(int teamId);//查询指定球队信息
