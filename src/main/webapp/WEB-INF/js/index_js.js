d7083870e6cae27f675391bde7c93b87ffb03dd7
Mr.Jiang <2679417790@qq.com>
2018-10-22 17:08:44 +0800

    增加前端代码结构以及登陆和注册以及页面隐藏和显示的js代码

---
+++ src/main/webapp/WEB-INF/js/index_js.js
@@ -0,0 +1,18 @@
+function show() {
+    document.getElementById("logup").style.display = "none";
+
+    document.getElementById("login").style.display = "block";
+}
+
+function Login() {
+    document.getElementById("logup").style.display = "none";
+
+    document.getElementById("login").style.display = "block";
+
+}
+
+function Regist() {
+    document.getElementById("logup").style.display = "block";
+
+    document.getElementById("login").style.display = "none";
+}
