cf28a01bb7648a2fef548aee57a32845fc16009e
weixingfu <wxffxw.664587@gmail.com>
2018-10-25 20:41:40 +0800

    更新配置文件

--- src/test/java/org/octopusbaby/basketball/BaseTest.java
+++ src/test/java/org/octopusbaby/basketball/BaseTest.java
@@ -8,7 +8,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * 配置spring和junit整合，junit启动时加载springIOC容器 spring-test,junit
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:spring/spring-dao.xml",
-        "classpath:spring/spring-service.xml"}) //junit读取spring配置文件
+@ContextConfiguration({"classpath:spring/spring-mybatis.xml"}) //junit读取spring配置文件
 public class BaseTest {
 }
