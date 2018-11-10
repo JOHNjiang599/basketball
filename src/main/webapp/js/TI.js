3bf974f6fd74c4ad733be5cd44d9783b68d772dd
JOHN <37733548+johnjiang599@users.noreply.github.com>
2018-11-10 22:51:14 +0800

    complete the teaminfo page and ajax logitec

--- src/main/webapp/js/TI.js
+++ src/main/webapp/js/TI.js
@@ -1,9 +1,17 @@
+var Url = window.location.href;
+var Data = Url.split("=");
+var Name = decodeURI(Data[1]);
+
+
+
+
+
 //从后台获取信息，把球队的信息填写到框框里面
-$(function TeamI() {
+$(function () {
     $.ajax({
         //与后台进行连接，接收json
         type: "get",
-        url: "nonono",
+        url: "../js/text",
         async: true,
         success: function (result) {
             var m = "";//用于后续的append
@@ -67,22 +75,24 @@ $(function TeamI() {
 function Change(ID) {
     var a = $("#" + ID).parent();
     var b = a.siblings();
-    b[0].innerHTML = "<input type='text' value='" + b[0].innerText + "'/>";
-    b[1].innerHTML = "<input type='text' value='" + b[1].innerText + "'/>";
-    b[2].innerHTML = "<input type='text' value='" + b[2].innerText + "'/>";
+    b[0].innerHTML = "<input type='text' id='MF' value='" + b[0].innerText + "'/>";
+    b[1].innerHTML = "<input type='text' id='MI' value='" + b[1].innerText + "'/>";
+    b[2].innerHTML = "<input type='text' id='MN' value='" + b[2].innerText + "'/>";
     var c = a.innerText;
     console.log(c);
-    a.innerHTML = "<input type='button' id='save' onclick='Save()' value='保存'>" +
+    a[0].innerHTML = "<input type='button' id='save' onclick='Save()' value='保存'>" +
         "<input type='button' id='cancel' onclick='Can()' value='取消'>";
 }
 
 function Del(ID) {
     var a = $("#" + ID).parent();
     var b = a.siblings();
-    var member = {memberId: b[1].innerText};
     $.ajax({
         type: "POST",
-        data: $.param(member),
+        data: {
+            "memberId":b[1].innerText,
+            "teamId":Name
+        },
         url: "",
         dataType: "json",
         success: function (result) {
@@ -92,22 +102,72 @@ function Del(ID) {
             }
             else {
                 alert("删除失败，请重试");
-                ;
                 return false;
             }
+        },
+        error:function () {
+            alert("服务器出错，请稍后再试");
+            return false;
         }
     })
 }
 
 
 function Add() {
-
+//    先进行表格的增加，然后把按钮变成保存和取消，之后进行input，之后在进行保存和取消的逻辑。但是取消用CAN()函数。是remove
+    var newmember = "<tr><td><input type='text' id='MF' value='mF'/></td>" +
+        "<td><input type='text' id='MI' value='mI'/></td>" +
+        "<td><input type='text' id='MN' value='mN'/></td>" +
+        "<td><input type='button' id='save' onclick='Save()' value='保存'>" +
+        "<input type='button' id='cancel' onclick='Can()' value='取消'></td></tr>";
+    $("#TeamNumber").append(newmember);
+/*    $.ajax({
+        type:"POST",
+        url:"",
+        dataType:"json",
+        data:{
+            "memberFirstStart" : $("#MF").val(),
+            "memberId":$("#MI").val(),
+            "memberName":$("#MN").val(),
+            "teamId":Name
+        },
+        success:function (result) {
+            if (result.status === "1"){
+                window.location.reload();
+            }
+        },
+        error:function () {
+            alert("保存失败，请重试");
+            return false;
+        }
+    })*/
 }
 
 function Save() {
-
+    $.ajax({
+        type:"POST",
+        url:"",
+        data: {
+            "memberFirstStart" : $("#MF").val(),
+            "memberId":$("#MI").val(),
+            "memberName":$("#MN").val(),
+            "teamId":Name
+        },
+        dataType: "json",
+        success:function (result) {
+            if (result.status === "1"){
+                alert("保存成功");
+                window.location.reload();
+            }
+        },
+        error:function () {
+            alert("更改失败，请重试");
+            return false;
+        }
+    })
 }
 
-function Can() {
 
+function Can() {
+    window.location.reload();
 }
